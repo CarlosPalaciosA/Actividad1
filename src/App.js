201b2835912015204c260ea15c8f33ea36044f5d
CarlosPalaciosA <carlosepa06@hotmail.com>
2022-09-20 14:39:51 -0500

    Modificación proyecto defautl Reactjs

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code> Carlos Eduardo Palacios Aguilera</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/CarlosPalaciosA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Carlos Eduardo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
